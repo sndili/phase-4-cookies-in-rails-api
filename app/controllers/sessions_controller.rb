73e14c8cf0ce5166649b2ca48dd7436f5b7d79fa
Sylvia Ndili <ndilisylvia@gmail.com>
2022-11-29 11:15:19 +0300

    completed assignment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 class SessionsController < ApplicationController
   
   def index
+    # binding.pry
     session[:session_hello] ||= "World"
+    # session[:session_goodbye] ||= "goodbye"
     cookies[:cookies_hello] ||= "World"
+    # cookies[:cookies_goodbye] ||= "goodbye"
     render json: { session: session, cookies: cookies.to_hash }
+    # byebug    
+
   end
 
 end
